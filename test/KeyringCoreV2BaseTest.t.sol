60d6cd723951ffbc9610d4c470c32a1776681a7a
et3p <nelson.hunter.prendergast@gmail.com>
2024-07-02 15:14:48 -0400

    100% test coverage

--- test/KeyringCoreV2BaseTest.t.sol
+++ test/KeyringCoreV2BaseTest.t.sol
@@ -5,9 +5,7 @@ import "forge-std/Test.sol";
 import "../src/KeyringCoreV2.sol";
 
 contract KeyringCoreV2BaseTestRig is KeyringCoreV2Base {
-
     constructor() KeyringCoreV2Base() {}
-
 }
 
 contract KeyringCoreV2BaseTest is Test {
@@ -23,9 +21,7 @@ contract KeyringCoreV2BaseTest is Test {
         keyring = new KeyringCoreV2BaseTestRig();
         testKey = hex"abcd";
         testKeyHash = keyring.getKeyHash(testKey);
-        uint256 BASETIME = keyring.BASETIME();
-        uint256 EPOCHLENGTH = keyring.EPOCHLENGTH();
-        vm.warp(BASETIME+EPOCHLENGTH*10);
+        vm.warp(1704067200); // 01-01-2024 00:00:00
     }
 
     // Constructor and Admin Initialization Test
@@ -124,73 +120,104 @@ contract KeyringCoreV2BaseTest is Test {
         keyring.unblacklistEntity(1, nonAdmin);
     }
 
+    function testCredentialCreationExpired() public {
+        uint256 validFrom = block.timestamp;
+        uint256 validTo = validFrom + 2 days;
+        keyring.registerKey(validFrom, validTo, testKey);
+        uint256 createBefore = block.timestamp + 2 days;
+        uint256 validUntil = block.timestamp + 1 days;
+        vm.warp(block.timestamp + 1 days + 1 minutes);
+
+        vm.expectRevert(abi.encodeWithSelector(KeyringCoreV2Base.ErrInvalidCredential.selector, 1, nonAdmin, "EXP"));
+        keyring.createCredential{value: 1 ether}(nonAdmin, 1, createBefore, validUntil, 1 ether, testKey, "", "");
+    }
+
     // Credential Creation Tests
-    function testCreateCredential() public {
+    function testCreateCredentialOk() public {
         uint256 validFrom = block.timestamp;
         uint256 validTo = validFrom + 1 days;
-        uint32 currentEpoch = keyring.getCurrentEpoch();
-        uint32 nextEpoch = currentEpoch + 1;
-
         keyring.registerKey(validFrom, validTo, testKey);
-        keyring.createCredential{value: 1 ether}(nonAdmin, 1, currentEpoch, nextEpoch, 1 ether, testKey, "", "");
+        uint256 createBefore = block.timestamp + 5 minutes;
+        uint256 validUntil = block.timestamp + 1 days;
+
+        uint256 gasBefore = gasleft();
+        keyring.createCredential{value: 1 ether}(nonAdmin, 1, createBefore, validUntil, 1 ether, testKey, "", "");
+        uint256 gasAfter = gasleft();
         assertTrue(keyring.checkCredential(1, nonAdmin));
+        emit log_named_uint("Gas for COLD COST credential without RSA validation:", gasBefore - gasAfter);
+        
+        gasBefore = gasleft();
+        keyring.createCredential{value: 1 ether}(nonAdmin, 1, createBefore, validUntil, 1 ether, testKey, "", "");
+        gasAfter = gasleft();
+        emit log_named_uint("Gas for HOT COST credential without RSA validation:", gasBefore - gasAfter);
+        
+        gasBefore = gasleft();
+        keyring.createCredential(nonAdmin, 1, createBefore, validUntil, 0, testKey, "", "");
+        gasAfter = gasleft();
+        emit log_named_uint("Gas for HOT NO-COST credential without RSA validation:", gasBefore - gasAfter);
+        
+        gasBefore = gasleft();
+        keyring.checkCredential(1, nonAdmin);
+        gasAfter = gasleft();
+        emit log_named_uint("Gas for checkCredential:", gasBefore - gasAfter);
     }
 
     function testCreateCredentialInsufficientPayment() public {
         uint256 validFrom = block.timestamp;
         uint256 validTo = validFrom + 1 days;
-        uint32 currentEpoch = keyring.getCurrentEpoch();
-        uint32 nextEpoch = currentEpoch + 1;
-
         keyring.registerKey(validFrom, validTo, testKey);
+        uint256 createBefore = block.timestamp + 5 minutes;
+        uint256 validUntil = block.timestamp + 1 days;
+
         vm.expectRevert(abi.encodeWithSelector(KeyringCoreV2Base.ErrInvalidCredential.selector, 1, nonAdmin, "VAL"));
-        keyring.createCredential{value: 0.5 ether}(nonAdmin, 1, currentEpoch, nextEpoch, 1 ether, testKey, "", "");
+        keyring.createCredential{value: 0.5 ether}(nonAdmin, 1, createBefore, validUntil, 1 ether, testKey, "", "");
     }
 
     function testCreateCredentialInvalidKey() public {
         uint256 validFrom = block.timestamp;
-        uint256 validTo = validFrom - 1 days;
-        uint32 currentEpoch = keyring.getCurrentEpoch();
-        uint32 nextEpoch = currentEpoch + 1;
+        uint256 validTo = validFrom + 1 days;
+        keyring.registerKey(validFrom, validTo, testKey);
+        vm.warp(block.timestamp + 2 days);
+        uint256 createBefore = block.timestamp + 5 minutes;
+        uint256 validUntil = block.timestamp + 1 days;
 
         vm.expectRevert(abi.encodeWithSelector(KeyringCoreV2Base.ErrInvalidCredential.selector, 1, nonAdmin, "BDK"));
-        keyring.createCredential{value: 1 ether}(nonAdmin, 1, currentEpoch, nextEpoch, 1 ether, testKey, "", "");
+        keyring.createCredential{value: 1 ether}(nonAdmin, 1, createBefore, validUntil, 1 ether, testKey, "", "");
     }
 
-    function testCreateCredentialOutsideEpoch() public {
+    function testCreateCredentialOutsideCreateBefore() public {
         uint256 validFrom = block.timestamp;
         uint256 validTo = validFrom + 1 days;
-        uint32 currentEpoch = keyring.getCurrentEpoch();
-        uint32 nextEpoch = currentEpoch + 1;
-
         keyring.registerKey(validFrom, validTo, testKey);
-        vm.warp(keyring.getTimeForEndOfEpoch(currentEpoch) + 5);
+        uint256 createBefore = block.timestamp - 1 days;
+        uint256 validUntil = block.timestamp + 1 days;
+
         vm.expectRevert(abi.encodeWithSelector(KeyringCoreV2Base.ErrInvalidCredential.selector, 1, nonAdmin, "EPO"));
-        keyring.createCredential{value: 1 ether}(nonAdmin, 1, currentEpoch-2, nextEpoch-1, 1 ether, testKey, "", "");
+        keyring.createCredential{value: 1 ether}(nonAdmin, 1, createBefore, validUntil, 1 ether, testKey, "", "");
     }
 
     function testCreateCredentialBlacklistedEntity() public {
         uint256 validFrom = block.timestamp;
         uint256 validTo = validFrom + 1 days;
-        uint32 currentEpoch = keyring.getCurrentEpoch();
-        uint32 nextEpoch = currentEpoch + 1;
-
         keyring.registerKey(validFrom, validTo, testKey);
         keyring.blacklistEntity(1, nonAdmin);
+        uint256 createBefore = block.timestamp + 5 minutes;
+        uint256 validUntil = block.timestamp + 1 days;
+
         vm.expectRevert(abi.encodeWithSelector(KeyringCoreV2Base.ErrInvalidCredential.selector, 1, nonAdmin, "BLK"));
-        keyring.createCredential{value: 1 ether}(nonAdmin, 1, currentEpoch, nextEpoch, 1 ether, testKey, "", "");
+        keyring.createCredential{value: 1 ether}(nonAdmin, 1, createBefore, validUntil, 1 ether, testKey, "", "");
     }
 
     function testCreateCredentialExpirationInPast() public {
         uint256 validFrom = block.timestamp;
         uint256 validTo = validFrom + 1 days;
-        uint32 currentEpoch = keyring.getCurrentEpoch();
-        uint32 nextEpoch = currentEpoch;
-
         keyring.registerKey(validFrom, validTo, testKey);
-        vm.warp(keyring.getTimeForEndOfEpoch(currentEpoch+1));
-        vm.expectRevert(abi.encodeWithSelector(KeyringCoreV2Base.ErrInvalidCredential.selector, 1, nonAdmin, "EXP"));
-        keyring.createCredential{value: 1 ether}(nonAdmin, 1, currentEpoch, nextEpoch, 1 ether, testKey, "", "");
+        uint256 createBefore = block.timestamp + 2 days;
+        uint256 validUntil = block.timestamp + 1 days;
+
+        vm.expectRevert(abi.encodeWithSelector(KeyringCoreV2Base.ErrInvalidCredential.selector, 1, nonAdmin, "BDK"));
+        vm.warp(block.timestamp + 2 days);
+        keyring.createCredential{value: 1 ether}(nonAdmin, 1, createBefore, validUntil, 1 ether, testKey, "", "");
     }
 
     // Fee Collection Tests
@@ -198,7 +225,10 @@ contract KeyringCoreV2BaseTest is Test {
         uint256 validFrom = block.timestamp;
         uint256 validTo = validFrom + 1 days;
         keyring.registerKey(validFrom, validTo, testKey);
-        keyring.createCredential{value: 1 ether}(nonAdmin, 1, keyring.getCurrentEpoch(), keyring.getCurrentEpoch() + 1, 1 ether, testKey, "", "");
+        uint256 createBefore = block.timestamp + 5 minutes;
+        uint256 validUntil = block.timestamp + 1 days;
+
+        keyring.createCredential{value: 1 ether}(nonAdmin, 1, createBefore, validUntil, 1 ether, testKey, "", "");
         uint256 balanceBefore = address(this).balance;
         uint256 keyringBalanceBefore = address(keyring).balance;
         keyring.collectFees(admin);
@@ -217,29 +247,6 @@ contract KeyringCoreV2BaseTest is Test {
     // View Function Tests
     function testAdmin() public {
         assertEq(keyring.admin(), admin);
-    }
-
-    function testGetTimeForEndOfEpoch() public {
-        uint256 currentTime = block.timestamp;
-        uint256 expectedEndTime = currentTime + keyring.EPOCHLENGTH();
-        assertEq(keyring.getTimeForEndOfEpoch(keyring.getCurrentEpoch()), expectedEndTime);
-    }
-
-    function testGetTimeForStartOfEpoch() public {
-        uint256 currentTime = block.timestamp;
-        uint256 expectedStartTime = currentTime - (currentTime % keyring.EPOCHLENGTH());
-        assertEq(keyring.getTimeForStartOfEpoch(keyring.getCurrentEpoch()), expectedStartTime);
-    }
-
-    function testGetCurrentEpoch() public {
-        uint32 expectedEpoch = keyring.getEpochForTime(block.timestamp);
-        assertEq(keyring.getCurrentEpoch(), expectedEpoch);
-    }
-
-    function testGetEpochForTime() public {
-        uint256 currentTime = block.timestamp;
-        uint32 expectedEpoch = uint32((currentTime - keyring.BASETIME()) / keyring.EPOCHLENGTH());
-        assertEq(keyring.getEpochForTime(currentTime), expectedEpoch);
     }
 
     function testGetKeyHash() public {
@@ -290,16 +297,21 @@ contract KeyringCoreV2BaseTest is Test {
         uint256 validFrom = block.timestamp;
         uint256 validTo = validFrom + 1 days;
         keyring.registerKey(validFrom, validTo, testKey);
-        uint256 expTime = keyring.getTimeForEndOfEpoch(keyring.getCurrentEpoch()+1);
-        keyring.createCredential{value: 1 ether}(nonAdmin, 1, keyring.getCurrentEpoch(), keyring.getCurrentEpoch() + 1, 1 ether, testKey, "", "");
-        assertEq(keyring.entityExp(1, nonAdmin), expTime);
+        uint256 createBefore = block.timestamp + 5 minutes;
+        uint256 validUntil = block.timestamp + 1 days;
+
+        keyring.createCredential{value: 1 ether}(nonAdmin, 1, createBefore, validUntil, 1 ether, testKey, "", "");
+        assertEq(keyring.entityExp(1, nonAdmin), validUntil);
     }
 
     function testEntityData() public {
         uint256 validFrom = block.timestamp;
         uint256 validTo = validFrom + 1 days;
         keyring.registerKey(validFrom, validTo, testKey);
-        keyring.createCredential{value: 1 ether}(nonAdmin, 1, keyring.getCurrentEpoch(), keyring.getCurrentEpoch() + 1, 1 ether, testKey, "", "");
+        uint256 createBefore = block.timestamp + 5 minutes;
+        uint256 validUntil = block.timestamp + 1 days;
+
+        keyring.createCredential{value: 1 ether}(nonAdmin, 1, createBefore, validUntil, 1 ether, testKey, "", "");
         KeyringCoreV2Base.EntityData memory ed = keyring.entityData(1, nonAdmin);
         assertTrue(ed.exp == keyring.entityExp(1, nonAdmin));
         assertTrue(ed.blacklisted == false);
@@ -309,7 +321,10 @@ contract KeyringCoreV2BaseTest is Test {
         uint256 validFrom = block.timestamp;
         uint256 validTo = validFrom + 1 days;
         keyring.registerKey(validFrom, validTo, testKey);
-        keyring.createCredential{value: 1 ether}(nonAdmin, 1, keyring.getCurrentEpoch(), keyring.getCurrentEpoch() + 1, 1 ether, testKey, "", "");
+        uint256 createBefore = block.timestamp + 5 minutes;
+        uint256 validUntil = block.timestamp + 1 days;
+
+        keyring.createCredential{value: 1 ether}(nonAdmin, 1, createBefore, validUntil, 1 ether, testKey, "", "");
         assertTrue(keyring.checkCredential(1, nonAdmin));
     }
 
@@ -317,8 +332,11 @@ contract KeyringCoreV2BaseTest is Test {
         uint256 validFrom = block.timestamp;
         uint256 validTo = validFrom + 1 days;
         keyring.registerKey(validFrom, validTo, testKey);
-        keyring.createCredential{value: 1 ether}(nonAdmin, 1, keyring.getCurrentEpoch() - 2, keyring.getCurrentEpoch() - 1, 1 ether, testKey, "", "");
-        uint256 ts = keyring.getTimeForEndOfEpoch(keyring.getCurrentEpoch());
+        uint256 createBefore = block.timestamp + 5 minutes;
+        uint256 validUntil = block.timestamp + 1 days;
+
+        keyring.createCredential{value: 1 ether}(nonAdmin, 1, createBefore, validUntil, 1 ether, testKey, "", "");
+        uint256 ts = block.timestamp + 2 days;
         vm.warp(ts + 1);
         assertFalse(keyring.checkCredential(1, nonAdmin)); // Should fail due to expiration
     }
@@ -327,7 +345,10 @@ contract KeyringCoreV2BaseTest is Test {
         uint256 validFrom = block.timestamp;
         uint256 validTo = validFrom + 1 days;
         keyring.registerKey(validFrom, validTo, testKey);
-        keyring.createCredential{value: 1 ether}(nonAdmin, 1, keyring.getCurrentEpoch(), keyring.getCurrentEpoch() + 1, 1 ether, testKey, "", "");
+        uint256 createBefore = block.timestamp + 5 minutes;
+        uint256 validUntil = block.timestamp + 1 days;
+
+        keyring.createCredential{value: 1 ether}(nonAdmin, 1, createBefore, validUntil, 1 ether, testKey, "", "");
         keyring.blacklistEntity(1, nonAdmin);
         assertFalse(keyring.checkCredential(1, nonAdmin)); // Should fail due to blacklisting
     }
