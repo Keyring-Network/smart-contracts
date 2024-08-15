21f338728c8a53d0114c646a83a7ad6b59a9334d
et3p <nelson.hunter.prendergast@gmail.com>
2024-08-15 11:00:33 -0400

    added json loading to RSA test vecotrs

--- test/RsaVerifyOptimizedTest.t.sol
+++ test/RsaVerifyOptimizedTest.t.sol
@@ -22,32 +22,46 @@ contract RsaVerifyOptimizedTestRig is RsaVerifyOptimized {
 contract KeyringCoreV2UnsafeTest is Test {
     RsaVerifyOptimizedTestRig internal keyring;
 
+    // MUST BE IN ALPHABETICAL ORDER OR JSON WILL NOT PARSE!
+    struct TestVector {
+        bytes backdoor;
+        uint256 cost;
+        uint256 createBefore;
+        bool expected;
+        bytes key;
+        uint256 policyId;
+        bytes signature;
+        address tradingAddress;
+        uint256 validUntil;
+    }
+
+    struct TestVectors {
+        TestVector[] vectors;
+    }
+
     function setUp() public {
         keyring = new RsaVerifyOptimizedTestRig();
     }
 
     function testVerify() public {
-        // Trading Address: 0x0123456789abcDEF0123456789abCDef01234567
-        // Policy ID: 123456
-        // Create Before: 1625247600
-        // Valid Until: 1627849600
-        // Cost: 1000000000000000000
-        // Backdoor: 6578616d706c655f6261636b646f6f725f64617461
-        // Encoded Message: 0123456789abcdef0123456789abcdef012345670001e24060df4f70610703800000000000000000000000000de0b6b3a76400006578616d706c655f6261636b646f6f725f64617461
-        // Key:       bf5f1afa62dda3a4184bfccaaf0efd4fec28aa13addbc5bb5a4c4fa7b21a9158d68d258e61278e84b0beb685b9d6408b88df36643f8e065444688a5f9d5aab0d48320e4c36b7766eee27d764e5d00296455c37557107603290df43991a344c8b8e26df06c6abd5f1eff0b3890ab3e7b08bf8aa59b30e28707679b7ae447f9671e599432427baae0c488252daf39673b8b2ac28e9e740030871f78aa55f7c334082f45878c5a3c2a926ae430ab34f8ec59291d6aee2555d77089af3880acb993b75d0e744757f4ec069d63cefa44456ba0746cd7a8c520d22671fa6311c88a9be3f1471fa489efa5a52e9e077f3cd455bbf530d94922daf92f3ad1a33b97abb93
-        // Signature: bc1c1769cde42e7f699ed69f1b2af1bf2d44e92f80ede553a76835f69187037594c81f6f4894fb2da67ba5c1a626e051486ae32de6f4c529fdd2b6c0ff1dd03479c3ec715642ad4c7d432717acfd612e1547aa67df28a9bba91a5f7e4cde010b749526a19423fe5c6c3e176340607cdab13fe9c2ab03c584dcf62d49423a826e
-        address tradingAddress = 0x0123456789abcDEF0123456789abCDef01234567;
-        uint256 policyId = 123456;
-        uint256 createBefore = 1625247600;
-        uint256 validUntil = 1627849600;
-        uint256 cost = 1000000000000000000;
-        bytes memory backdoor = hex"6578616d706c655f6261636b646f6f725f64617461";
-        bytes memory key = hex"c28aa13addbc5bb5a4c4fa7b21a9158d68d258e61278e84b0beb685b9d6408b88df36643f8e065444688a5f9d5aab0d48320e4c36b7766eee27d764e5d00296455c37557107603290df43991a344c8b8e26df06c6abd5f1eff0b3890ab3e7b08bf8aa59b30e28707679b7ae447f9671e599432427baae0c488252daf39673b8b";
-        bytes memory signature = hex"502f5ac1ba587e3eaff95c62fe5c4e05fb1bf7243297b7b9c1459d83c1898545114a74fef5ea5b98641292cd167a680eb346c9d3384e0bffb929364091d8b73fad82a9f97550e11b8eaf1abf32347a8ebab5fc77480dae381828cf50ab066ca0b41f2ecd3b1228c25888d07db4d30c3dbe7a691d4d180a2a0856de32120824a8";
-        uint256 gasBefore = gasleft();
-        bool result = keyring.verify(tradingAddress, policyId, createBefore, validUntil, cost, key, signature, backdoor);
-        uint256 gasAfter = gasleft();
-        emit log_named_uint("Gas Used:", gasBefore - gasAfter);
-        assertTrue(result);
+        string memory root = vm.projectRoot();
+        string memory path = string.concat(root, "/test/rsa_vectors/test_vectors.json");
+        string memory json = vm.readFile(path);
+        bytes memory data = vm.parseJson(json);
+        TestVectors memory testVectors = abi.decode(data, (TestVectors));
+        for (uint256 i = 0; i < testVectors.vectors.length; i++) {
+            TestVector memory vector = testVectors.vectors[i];
+            bool result = keyring.verify(
+                vector.tradingAddress,
+                vector.policyId,
+                vector.createBefore,
+                vector.validUntil,
+                vector.cost,
+                vector.key,
+                vector.signature,
+                vector.backdoor
+            );
+            assertTrue(result == vector.expected);
+        }
     }
 }
--- utils/generateTestVectorRSA.py
+++ utils/generateTestVectorRSA.py
@@ -4,17 +4,17 @@ from Crypto.Hash import SHA256
 from eth_abi.packed import encode_packed
 from eth_utils import to_bytes
 from binascii import hexlify
+import json
 
-def pack_message(trading_address, policy_id, creat_before, valid_until, cost, backdoor):
+def pack_message(tradingAddress, policyId, createBefore, validUntil, cost, backdoor):
     # Convert the address to bytes
-    trading_address_bytes = to_bytes(hexstr=trading_address)
+    tradingAddress_bytes = to_bytes(hexstr=tradingAddress)
     
     # Encode the values according to the specified types
     message = encode_packed(
         ['address', 'uint8', 'uint24', 'uint32', 'uint32', 'uint160', 'bytes'],
-        [trading_address_bytes, 0, policy_id, creat_before, valid_until, cost, backdoor]
+        [tradingAddress_bytes, 0, policyId, createBefore, validUntil, cost, backdoor]
     )
-    print(hexlify(message))
     return message
 
 # Function to generate RSA key with public exponent 3
@@ -22,8 +22,8 @@ def generate_rsa_key_with_exponent_3():
     key = RSA.generate(1024, e=3)
     return key.export_key(), key.publickey().export_key()
 
-def encode_data(trading_address, policy_id, creat_before, valid_until, cost, backdoor):
-    return pack_message(trading_address, policy_id, creat_before, valid_until, cost, backdoor)
+def encode_data(tradingAddress, policyId, createBefore, validUntil, cost, backdoor):
+    return pack_message(tradingAddress, policyId, createBefore, validUntil, cost, backdoor)
 
 def sign_message(message, private_key):
     # Hash the message using SHA256
@@ -34,45 +34,59 @@ def sign_message(message, private_key):
     signature = pkcs1_15.new(rsa_key).sign(hash_obj)
     return signature
 
-# Example usage
-trading_address = "0x0123456789abcDEF0123456789abCDef01234567"
-policy_id = 123456
-creat_before = 1625247600
-valid_until = 1627849600
-cost = 1000000000000000000  # Example cost in wei
-backdoor = b"example_backdoor_data"
-
-# Generate a 1024-bit RSA key with public exponent 3 (for demonstration purposes)
-private_key, public_key = generate_rsa_key_with_exponent_3()
-
-# Encode the data
-encoded_message = encode_data(trading_address, policy_id, creat_before, valid_until, cost, backdoor)
-
-# Sign the encoded message
-signature = sign_message(encoded_message, private_key)
-
-rsa_key = RSA.import_key(public_key)
-
-# Print all properly encoded data
-print(f"Trading Address: {trading_address}")
-print(f"Policy ID: {policy_id}")
-print(f"Create Before: {creat_before}")
-print(f"Valid Until: {valid_until}")
-print(f"Cost: {cost}")
-print(f"Backdoor: {backdoor.hex()}")
-print(f"Encoded Message: {encoded_message.hex()}")
-print(f"Key: {hex(rsa_key.public_key().n)[2:]}")
-print(f"Signature: {signature.hex()}")
-
-# For verification (optional)
+# For verification
 def verify_signature(public_key, message, signature):
     rsa_key = RSA.import_key(public_key)
     hash_obj = SHA256.new(message)
     try:
         pkcs1_15.new(rsa_key).verify(hash_obj, signature)
-        print("The signature is valid.")
     except (ValueError, TypeError):
         print("The signature is not valid.")
 
-# Verify the signature (optional)
-verify_signature(public_key, encoded_message, signature)
+def generateVector():
+    # Example usage
+    tradingAddress = "0x0123456789abcDEF0123456789abCDef01234567"
+    policyId = 123456
+    createBefore = 1625247600
+    validUntil = 1627849600
+    cost = 1000000000000000000  # Example cost in wei
+    backdoor = b"example_backdoor_data"
+
+    # Generate a 1024-bit RSA key with public exponent 3 (for demonstration purposes)
+    private_key, public_key = generate_rsa_key_with_exponent_3()
+
+    # Encode the data
+    encoded_message = encode_data(tradingAddress, policyId, createBefore, validUntil, cost, backdoor)
+
+    # Sign the encoded message
+    signature = sign_message(encoded_message, private_key)
+
+    rsa_key = RSA.import_key(public_key)
+
+    # Verify the signature (optional)
+    verify_signature(public_key, encoded_message, signature)
+    return {
+        "tradingAddress": tradingAddress,
+        "policyId": policyId,
+        "createBefore": createBefore,
+        "validUntil": validUntil,
+        "cost": cost,
+        "backdoor": "0x" + backdoor.hex(),
+        "key": "0x" + hex(rsa_key.public_key().n)[2:],
+        "signature": "0x" + signature.hex(),
+        "expected": True
+    }
+
+def writeVectors(vectors):
+    with open('test_vectors.json', "w") as f:
+        f.write(json.dumps(vectors))
+        
+def makeVectors():
+    vectors = {"vectors": [None]*100}
+    for i in range(100):
+        vectors["vectors"][i] = generateVector()
+    return vectors
+
+vectors = makeVectors()
+writeVectors(vectors)
+    
