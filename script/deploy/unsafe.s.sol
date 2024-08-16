c2c34c0e1c018a42bfd2642228853f324fb15816
et3p <nelson.hunter.prendergast@gmail.com>
2024-08-16 08:27:39 -0400

    nonupgradable deployer

--- script/deploy/unsafe.s.sol
+++ script/deploy/unsafe.s.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "../src/unsafe/KeyringCoreV2Unsafe.sol";
+import "../../src/unsafe/KeyringCoreV2Unsafe.sol";
 
 contract DeployScript is Script {
     function run() external {
