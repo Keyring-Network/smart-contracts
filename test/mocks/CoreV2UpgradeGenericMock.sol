76a841c3dd6ed6b16b2900428318dc9591855daa
Florian de Miramon <florian@idm.io>
2025-02-21 11:55:07 +0100

    Fix tests

--- test/mocks/CoreV2UpgradeGenericMock.sol
+++ test/mocks/CoreV2UpgradeGenericMock.sol
@@ -5,10 +5,21 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "../../src/ICredentialCache.sol";
+import "../../src/interfaces/ICoreV2Base.sol";
 
 
-contract CoreV2UpgradeGenericMock is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+
+contract CoreV2UpgradeGenericMock is ICoreV2Base, Initializable, OwnableUpgradeable, UUPSUpgradeable {
         
+    /// @dev Address of the admin.
+    address internal _admin;
+
+    /// @dev Mapping from key hash to key entry.
+    mapping(bytes32 => KeyEntry) internal _keys;
+
+    /// @dev Mapping from policy ID and address to entity data.
+    mapping(uint256 => mapping(address => EntityData)) internal _entityData;
+
     /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
     uint64 public immutable VERSION;
 
